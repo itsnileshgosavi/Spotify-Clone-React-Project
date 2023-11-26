94f99a699a252c745becb58b4f83c061854aeea7
Nilesh <nbgosavi5@gmail.com>
2023-11-26 23:08:45 +0530

    changing client id

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export default function Login() {
   const handleClick = () => {
-    const clientId = "057d420adec64095904ebee27e73f91d";
+    const clientId = "17fcb8e4f5e547d1b4d76100bb0550d0";
     const redirectURL = "https://itsnileshgosavi.github.io/Spotify-Clone-React-Project/";
     const apiurl = "https://accounts.spotify.com/authorize";
     const scope = [
