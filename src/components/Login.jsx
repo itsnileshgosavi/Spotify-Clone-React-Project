4666cd66e10c855bf7467d22c41ca7cfcdbe7dfa
Nilesh <nbgosavi5@gmail.com>
2023-11-26 22:37:10 +0530

    adding deployment changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export default function Login() {
   const handleClick = () => {
     const clientId = "057d420adec64095904ebee27e73f91d";
-    const redirectURL = "http://localhost:3000/";
+    const redirectURL = "https://itsnileshgosavi.github.io/Spotify-Clone-React-Project/";
     const apiurl = "https://accounts.spotify.com/authorize";
     const scope = [
       "user-read-email",
